7d31f60d1c045e861b6fd9f70a0fa4b55d01cddd
NoelBrazza <160472950+NoelBrazza@users.noreply.github.com>
2024-05-24 17:06:00 +0300

    Update weather.js

--- commandes/weather.js
+++ commandes/weather.js
@@ -10,17 +10,17 @@ if (!text) return repondre("Give me location!!");
                 `https://api.openweathermap.org/data/2.5/weather?q=${text}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=en`
             );
             let textw = "";
-            textw += `*🌟Weather of  ${text}*\n\n`;
+            textw += `*❄️ Weather of  ${text}*\n\n`;
             textw += `*Weather:-* ${wdata.data.weather[0].main}\n`;
-            textw += `*Description:-* ${wdata.data.weather[0].description}\n`;
-            textw += `*Avg Temp:-* ${wdata.data.main.temp}\n`;
-            textw += `*Feels Like:-* ${wdata.data.main.feels_like}\n`;
-            textw += `*Pressure:-* ${wdata.data.main.pressure}\n`;
-            textw += `*Humidity:-* ${wdata.data.main.humidity}\n`;
-            textw += `*Humidity:-* ${wdata.data.wind.speed}\n`;
-            textw += `*Latitude:-* ${wdata.data.coord.lat}\n`;
-            textw += `*Longitude:-* ${wdata.data.coord.lon}\n`;
-            textw += `*Country:-* ${wdata.data.sys.country}\n`;
+            textw += `*📝 Description:-* ${wdata.data.weather[0].description}\n`;
+            textw += `*🌡️Avg Temp:-* ${wdata.data.main.temp}\n`;
+            textw += `*🌡️ Feels Like:-* ${wdata.data.main.feels_like}\n`;
+            textw += `*☁️ Pressure:-* ${wdata.data.main.pressure}\n`;
+            textw += `*❄️ Humidity:-* ${wdata.data.main.humidity}\n`;
+            textw += `*❄️ Humidity:-* ${wdata.data.wind.speed}\n`;
+            textw += `*🌐 Latitude:-* ${wdata.data.coord.lat}\n`;
+            textw += `*🌐 Longitude:-* ${wdata.data.coord.lon}\n`;
+            textw += `*🗺️ Country:-* ${wdata.data.sys.country}\n`;
 
             dest.sendMessage(
                 zk.chat, {
